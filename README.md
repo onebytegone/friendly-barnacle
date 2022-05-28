ffef8f858f14b03ec315725c09e0f012159d0c0f
Ethan Smith <ethan@onebytegone.com>
2022-05-28 17:42:57 -0400

    refactor: move files after autogen

---
+++ README.md
@@ -0,0 +1,10 @@
+# friendly-barnacle
+
+## What?
+
+## Why?
+
+## License
+
+This software is released under the MIT license. See [the license
+file](LICENSE) for more details.
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,17 @@
-import Example from './Example';
+import express from 'express';
+import http from 'http';
 
-export {
-   Example,
-};
+const app = express(),
+      port = process.env.PORT || 3000, // eslint-disable-line no-process-env
+      server = new http.Server(app);
+
+app.get('/', (_req, res) => {
+   res.json({
+      pong: new Date(),
+   });
+});
+
+server.listen(port, () => {
+   // eslint-disable-next-line no-console
+   console.log(`Listening on port ${port}`);
+});
